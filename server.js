c4932300a7cb987b2b96a61ebc5cc82ed5f335f1
florin-mavroian <f-mavroian@list.co.jp>
2020-08-20 09:43:07 +0900

    handling update profile requests

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/profile/:id', (req, res)=> { profile.handleProfileUpdate(req, res, db)})
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
