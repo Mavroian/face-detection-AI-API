ea9388e3daa2794b184e98035eccbaeafa731365
florin-mavroian <f-mavroian@list.co.jp>
2020-08-20 09:44:08 +0900

    added 2 more columns to db

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 
 BEGIN TRANSACTION;
 
-INSERT INTO users ( name, email, entries, joined ) values('Miha', 'miha@gmail.com', 5, '2018-01-01');
+INSERT INTO users ( name, email, entries, joined, age, pet ) values ('Miha', 'miha@gmail.com', 4, '2018-01-01', 34, 'Cat');
 INSERT INTO login ( hash, email) values ('$2a$10$YCxxLdUhMO15WINJI8F.Cu/i8Jo5RDjddq/VE618bBQDdvTuAi1GW', 'miha@gmail.com');
 COMMIT;
