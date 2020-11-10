c0c68665242e331ac3136431b93ce84a5046777e
florin-mavroian <f-mavroian@list.co.jp>
2020-11-10 14:22:36 +0900

    testing heroku

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ app.use(morgan())
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send("It is working") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log('app is running on port 3001');
+const PORT  = process.env.PORT || 3000
+app.listen(PORT, ()=> {
+  console.log(`App is running on port ${PORT}`);
 })
